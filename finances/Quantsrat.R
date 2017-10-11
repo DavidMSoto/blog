6d44ff1b759fae719522b2252f75a64b3f2c6533
davidmsoto <davidmontesoto@gmail.com>
2017-10-11 19:05:26 +0100

    removing files

--- finances/Quantsrat.R
+++
@@ -1,7 +0,0 @@
-
-#install.packages("quantstrat", repos="http://R-Forge.R-project.org", type="source")
-rm(list = ls())
-library()
-Sys.setenv(TZ = "UTC")
-
-install.packages(c("rj", "rj.gd"), repos="http://download.walware.de/rj-2.1")
